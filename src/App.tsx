f08e670c4d9b49c792fec6d4ef3150b71982fa1e
Saaketh <Saaketh@gmail.com>
2025-07-27 22:53:38 +0530

    TypeScript setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <h1 className="text-4xl font-bold text-center py-16">GameVault</h1>
+    </div>
+  );
+}
+
+export default App;
